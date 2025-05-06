618545478c323c2657772a511810f0cba367bb7d
sho-ikeda1 <sho.iked@bumail.net>
2025-05-06 19:05:54 +0700

    add scenes / some UI

---
+++ Assets/Scripts/Menu_Script.cs
@@ -0,0 +1,21 @@
+﻿using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+
+        // ถ้าเล่นใน Unity Editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
